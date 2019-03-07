a4deab96236d9e790c10230218154565b1f2ebc2
Hugh Willson <hugh@octonary.com>
2019-03-07 13:03:58 -0500

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # graphql-tools-stitching-errors
+
 Repro demonstrating issues with graphql-tools error stitching.
+
+## Steps
+
+```
+git clone https://github.com/hwillson/graphql-tools-stitching-errors.git
+cd graphql-tools-stitching-errors
+npm i
+npm start
+```
+
+**Test good response (no stitching):**
+
+```
+curl -X POST http://localhost:3000/graphql \
+  -H 'Content-Type:application/json' \
+  -H 'Accept:application/json' \
+  -d '{"query":"query{cars{inventory}}"}'
+```
+
+**Test bad response (stiching):**
+
+```
+curl -X POST http://localhost:3001/graphql \
+  -H 'Content-Type:application/json' \
+  -H 'Accept:application/json' \
+  -d '{"query":"query{cars{inventory}}"}'
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+const {
+  ApolloServer,
+  ApolloError,
+  makeExecutableSchema,
+  mergeSchemas,
+  gql
+} = require('apollo-server')
+
+const cars = [
+  {
+    __typename: 'Car',
+    make: 'Land Rover',
+    model: 'Range Rover',
+    year: 2015,
+  },
+];
+
+const schema = makeExecutableSchema({
+  typeDefs: gql`
+    type Car {
+      make: String!
+      model: String!
+      year: Int!
+      inventory: Int!
+    }
+
+    type Query {
+      cars: [Car]
+    }
+  `,
+  resolvers: {
+    Car: {
+      inventory() {
+        throw new ApolloError('test error', 'CUSTOM_ERROR_CODE', {
+          someExtraErrorProp: 'Something!',
+        });
+      }
+    },
+    Query: {
+      cars() {
+        return cars;
+      },
+    },
+  },
+});
+
+const goodApolloServer = new ApolloServer({ schema });
+goodApolloServer.listen(3000);
+
+const badApolloServer = new ApolloServer({
+  schema: mergeSchemas({ schemas: [schema] }),
+});
+badApolloServer.listen(3001);
